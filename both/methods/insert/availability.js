03c652234216dac60eec3d502bf0043bd5448be7
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-06-26 07:22:48 -0700

    handle inserting dates into database

---
+++ both/methods/insert/availability.js
@@ -0,0 +1,16 @@
+/*add the insert method that we'll ultimately call from the client to handle inserting our appointments into the database*/
+
+import {Meteor} from 'meteor/meteor';
+import {Appointments} from '../../../imports/api/availability.js';
+
+Meteor.methods({
+  indicateAvailability( availability ) {
+    check( availability, String ); //appointment equal to string, representing the date and time selected on the client
+
+    try {
+      return Availability.insert( { timestamp: availability } );
+    } catch( exception ) {
+      return exception;
+    }
+  }
+});
