0ac5f18392bd81468f5de8a1bbecd9fd4594c7c4
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-06-30 13:14:12 +0800

    Update availability.js

--- client/templates/availability.js
+++ client/templates/availability.js
@@ -2,12 +2,13 @@
   <div class="row">
     <div class="col-xs-12 col-sm-8 col-sm-offset-2">
       <h4 class="page-header">Select date and time unavailable</h4>
-      {{> addAppointment}}
-      <ul class="list-group appointments-list">
-        {{#each appointments}}
-          <li class="list-group-item">Blaze._globalHelpers['dateTime'] </li>
-        {{/each}}
-      </ul>
-    </div>
-  </div>
-</template>
+      //supposed to show a list of dates blocked
+      //{{> addAppointment}}
+      //<ul class="list-group appointments-list">
+        //{{#each appointments}}
+          //<li class="list-group-item">Blaze._globalHelpers['dateTime'] </li>
+        //{{/each}}
+     // </ul>
+   // </div>
+ // </div>
+//</template>
