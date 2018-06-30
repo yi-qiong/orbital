82b87754e15567942810d7a4bec8dd02600fce53
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-06-29 19:43:22 -0700

    update availability.js

--- imports/api/availability.js
+++ imports/api/availability.js
@@ -1,15 +1,48 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
-export const Avaliability = new Mongo.Collection( 'availability' );
 
-Avaliability.allow({
+export const Availability = new Mongo.Collection( 'availability' );
+
+Availability.allow({
   insert: () => false,
   update: () => false,
-  remove: () => false
+  remove: () => false                                
 });
 
-Avaliability.deny({
+Availability.deny({
   insert: () => true,
   update: () => true,
   remove: () => true
 });
+
+//if(Meteor.isServer) {
+	//Meteor.publish( 'availability', function() { 
+  //return Availability.find({ //only publish blocked out dates of user
+  	//$or: [
+  	//{private: {$ne: true}},
+  	//{owner: this.userId},
+  	//]
+  //})
+//});
+
+  Meteor.methods({
+  'indicateAvailability'( selectedDate, startTime, endTime ) {
+    check(selectedDate, String );
+    check(startTime, String);
+    check(endTime, String);
+    
+    Availability.insert({
+      date: selectedDate,
+      start: startTime,
+      end: endTime,
+      createdAt: new Date(),
+      //owner: Meteor.userId(),
+      //username: Meteor.user().username,
+    })
+  }
+});
+//}
+
+
