1f4b8f6fd64f915cae4df973d22f2e532a32a7c8
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-06-29 19:38:53 -0700

    update add-availability.js

--- client/templates/add-availability.js
+++ client/templates/add-availability.js
@@ -1,39 +1,62 @@
 import {Meteor} from 'meteor/meteor';
+import { Template } from 'meteor/templating';
 import './add-availability.html';
+import '../../imports/api/availability.js';
 
-Template.addAppointment.onRendered( () => { //initialize the datepicker when template rendered on screen 
-  $( '.datetimepicker' ).datetimepicker({
+Template.addAppointment.onRendered( () => {
+   $( '#datepicker' ).datetimepicker({
     timeZone: 'SGT',
-    useCurrent: true, //present the actual current date & time
-    minDate: new Date() //disable past dates and only show date starting from the current date
+    minDate: new Date(), //disable past dates and only show date starting from the current date
+    format: 'YYYY-MM-DD'
   });
 
-  Router.route('/availability', {
-  	template: 'addAppointment'
+  $('#start_time').datetimepicker({
+    format: 'hh:mm A',
+  });
+
+  $('#end_time').datetimepicker({
+    format: 'hh:mm A',
+  });
+
+//when select whole day, disable start time and end time. upon submit button, enable again
+
+Router.route('/availability', {
+    template: 'addAppointment'
   });
 });
 
+Template.addAppointment.onCreated( () => {
+  let template = Template.instance();
+  template.subscribe( 'availability' );
+});
+
 Template.addAppointment.events({
-  'submit form' ( event, template ) {
-    event.preventDefault();
+  
+  'click .wholeDay': function() {
+      
+      event.preventDefault();
+      $('#start_time').datetimepicker('disable');
+      $('#end_time').datetimepicker('disable');
+  },
 
-    let picker   = $( '.datetimepicker' ), //get input 
-        dateTime = picker.data( 'DateTimePicker' ).date();
 
-    if ( dateTime ) { //ensure there is a date input
-      let appointment = dateTime.format(); //format
+  'submit #add-appointment': function(event) {
+    
+       event.preventDefault();
+       $('#start_time').datetimepicker('enable');
+       $('#end_time').datetimepicker('enable');
 
-      Meteor.call( 'addAppointment', appointment, ( error, response ) => {
+    var selectedDate = event.target.datePicker.value;
+    var startTime = event.target.startTime.value;
+    var endTime = event.target.endTime.value;
+
+      Meteor.call( 'indicateAvailability', selectedDate, startTime, endTime, (error,response) => {
         if ( error ) {
           Bert.alert( error.reason, 'danger' );
         } else {
-          picker.val( '' );
           Bert.alert( 'Date blocked out', 'success' );
         }
       });
-    } else {
-      Bert.alert( 'Make sure to pick an appointment time!', 'danger' );
-    }
   }
 });
 
