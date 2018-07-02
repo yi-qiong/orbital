f4d2f41dedf430ad0252fff30a66d194224e57ba
yi-qiong <39292827+yi-qiong@users.noreply.github.com>
2018-07-02 14:05:07 +0800

    Create team.js

---
+++ server/team.js
@@ -0,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import '../imports/api/teams.js';
+
+Meteor.publish('userInfo', function () {
+  return Meteor.users.find(this.userId, {
+    fields: {
+      submittedTeamForm:1,
+      hall:1,
+      teams:1
+    }
+  });
+});
