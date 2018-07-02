a9317696799df1419ea21e3aadfadaad78484a7e
yi-qiong <39292827+yi-qiong@users.noreply.github.com>
2018-07-02 14:01:25 +0800

    Update teams.js

--- client/templates/teams.js
+++ client/templates/teams.js
@@ -1,7 +1,7 @@
 import {Meteor} from 'meteor/meteor';
 import {Template} from 'meteor/templating';
 import './teams.html';
-import '/lib/formMethods.js';
+import '../../imports/api/teams.js';
 
 
 ////////////////////////////////////////////////// form
