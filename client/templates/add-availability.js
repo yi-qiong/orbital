73d74016e7ccd6f63f46317232c2d87926297bc6
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-07-04 09:33:00 -0700

    update conditions for block out date

--- client/templates/add-availability.js
+++ client/templates/add-availability.js
@@ -19,11 +19,7 @@ Template.addAppointment.onRendered( () => {
   $('#end_time').datetimepicker({
     format: 'hh:mm A',
   });
-
-Router.route('/availability', {
-    template: 'addAppointment'
-  });
-}); //this one i try to delete but app crashes
+}); 
 
 Template.addAppointment.onCreated( () => {
   let template = Template.instance();
@@ -50,20 +46,55 @@ Template.addAppointment.events({
   'submit #add-appointment': function(event) {
     
        event.preventDefault();
-    
     //get value to be inserted into availability collection
     var selectedDate = event.target.datePicker.value;
     var startTime = event.target.startTime.value;
     var endTime = event.target.endTime.value;
 
-     //call method to insert into collection
-      Meteor.call( 'indicateAvailability', selectedDate, startTime, endTime, (error,response) => {
-        if ( error ) {
-          Bert.alert( error.reason, 'danger' );
-        } else {
-          Bert.alert( 'Date blocked out', 'success' );
-        }
-      });
+    console.log(selectedDate);
+    console.log(startTime);
+    console.log(endTime);
+
+    //if all fields empty, present error. 
+    if (selectedDate == "" && startTime == "" && endTime == "") {
+      Bert.alert('Form is empty!')
+    }
+
+    //did not choose date
+    if (selectedDate == "") {
+      Bert.alert('Date not indicated!')
+    }
+
+    //date and start time not indicated
+    if (selectedDate == "" && startTime == "") {
+      Bert.alert('Date and Start Time not indicated!')
+    }
+
+    //date and start time not indicated
+    if (selectedDate == "" && endTime == "") {
+      Bert.alert('Date and End Time not indicated!')
+    }
+
+    //did not indicate whole day not free and did not indicate start & end time
+    if (startTime == "" && endTime =="" && !($('#availability').checkbox('is checked'))) { 
+      Bert.alert('Start and End Time not indicated!')
+    }
+
+    //call insert method when user inputs all fields or indicate whole day not free
+    if (selectedDate != "" && startTime != "" && endTime != "" || selectedDate != "" && $('#availability').checkbox('is checked')) {
+          //popup message to let players confirm if they want to block out date
+          var confirmBlockDate = confirm("Are you sure you want to block out the date?");
+            if (confirmBlockDate == true) { //player confirm want to block out date
+              Meteor.call( 'indicateAvailability', selectedDate, startTime, endTime, (error,response) => {
+                if ( error ) {
+                  Bert.alert( error.reason, 'danger' );
+                } else {
+                  Bert.alert( 'Date blocked out', 'success' );
+                }
+              });
+
+          }
+      }
   }
 });
 
