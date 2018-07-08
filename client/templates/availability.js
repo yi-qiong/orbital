ffd0aa65b1a6d8868a7c63177b2be776b2891b0a
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-07-08 05:12:10 -0700

    update, can see the list now

--- client/templates/availability.js
+++ client/templates/availability.js
@@ -9,11 +9,41 @@ Template.appointments.onCreated(function() {
   });
 });
 
+Template.appointments.events({
+	'click #edit': function() {
+		//need to present the info in the picker then allow user to change the info
+	},
+
+	'click #delete': function() {
+		//delete the data 
+	}
+});
+
 Template.appointments.helpers({
   appointments() {
     let appointments = Availability.find({owner: Meteor.userId()}).fetch();
-    //if ( appointments ) {
-      return appointments;
-    //}
+    var datesList = new Array();
+   		$.each(appointments,function(key, value) {
+    	if (value.start == "" && value.end =="") {
+    		var blockedDate = "on " + value.date;
+    		console.log(blockedDate)
+    		datesList.push(blockedDate);
+    	} else {
+    		var blockedDate = value.date;
+    		var startTime = value.start;
+    		var endTime = value.end;
+    		var blockOut = "on " + blockedDate + " from " + startTime	+ " to " + endTime;
+    		console.log(blockOut)
+    		datesList.push(blockOut); 
+    	}
+    	
+    })
+   		return datesList;
+   	//console.log(appointments);
   }
 });
+
+//in html: output as a list 
+//when i click on an entry, i should be able to edit/delete the entry (use semantic animate button)
+//when click on edit button, the entry should reflect back in the datetimepicker
+//when click on the delete button, will just remove from the collection
