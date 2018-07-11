d694600ddc81db78932ab9f9c4d069e44140551b
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-07-11 10:13:21 -0700

    picker updated with input when clicked on edit

--- client/templates/add-availability.js
+++ client/templates/add-availability.js
@@ -9,45 +9,63 @@ Template.addAppointment.onRendered( () => {
   $( '#datepicker' ).datetimepicker({
     timeZone: 'SGT',
     minDate: new Date(), //disable past dates and only show date starting from the current date
-    format: 'YYYY-MM-DD'
+    format: 'DD/MM/YYYY'
   }); 
 
   $('#start_time').datetimepicker({
-    format: 'hh:mm A',
+    stepping: 15, //only allow time selection in intervals of 15 mins
+    format: 'HH:mm'
   });
 
   $('#end_time').datetimepicker({
-    format: 'hh:mm A',
+    stepping: 15, //only allow time selection in intervals of 15 mins
+    format: 'HH:mm'
   });
 
-  //this entire part doesnt work coz this onRendered is only called when the template is first rendered
-  //i nid to include this in a helper so it can run reactively
-  if (!Session.equals('selectedBlockOut',null)){//only executed if someone clicks #edit
-     date = Session.get('selectedBlockOut').date;
-    console.log("test");
+  document.getElementById("confirm").style.display="none"; //hide confirm edit button when first rendered
+  document.getElementById("cancel").style.display="none"; //hide cancel button when first rendered
+}); 
+
+
+Template.addAppointment.onCreated(function() {
+  this.autorun(() => {
+    this.subscribe('availability');
+    this.subscribe('userInfo');
+  });
+
+  this.autorun( () =>  { 
+    var isEdit = Session.get('selectedBlockOut');
+    if (isEdit != null) {//only executed if someone clicks #edit
+      document.getElementById("blockout").style.display="none";
+      document.getElementById("confirm").style.display="block";
+      document.getElementById("cancel").style.display="block";
+     var date = Session.get('selectedBlockOut').date;
     $('#datepicker').data('DateTimePicker').date(date);
     var start = Session.get('selectedBlockOut').start;
     $('#start_time').data('DateTimePicker').date(start);
     var end = Session.get('selectedBlockOut').end;
     $('#end_time').data('DateTimePicker').date(end);
-  } 
-  
-
-}); 
-
-Template.addAppointment.onCreated(function() {
-  this.autorun(() => {
-    this.subscribe('availability');
+    }
   });
 });
 
 Template.addAppointment.events({
 
+  'click #cancel': function() { //when player do not want to edit schedule
+    document.getElementById("blockout").style.display="block"; //show blockout button
+    document.getElementById("confirm").style.display="none"; //hide confirm edit button
+    document.getElementById("cancel").style.display="none"; //hide cancel button
+    $('#datepicker').data('DateTimePicker').date(null);
+    $('#start_time').data('DateTimePicker').date(null); 
+    $('#end_time').data('DateTimePicker').date(null); //set to placeholder value
+    Session.set('selectedBlockOut', null);
+  },
+
   'click #availability': function() { //toggle button events
     if ($('#availability').checkbox('is checked')) {
       event.preventDefault();
-      $('#start_time').data('DateTimePicker').date(null); //set to placeholder value
-      $('#end_time').data('DateTimePicker').date(null); //set to placeholder value
+      $('#start_time').data('DateTimePicker').date(null); 
+      $('#end_time').data('DateTimePicker').date(null); 
       //disable timepicker when button is toggled
       $('#start_time').datetimepicker('disable');
       $('#end_time').datetimepicker('disable');
@@ -59,7 +77,7 @@ Template.addAppointment.events({
   },
 
 
-  'submit #add-appointment': function(event) {
+  'submit #add-appointment': function(event) {//this works either when they block out or they confirm edit their schedule
     
     event.preventDefault();
     //get value to be inserted into availability collection
@@ -70,6 +88,7 @@ Template.addAppointment.events({
     console.log(selectedDate);
     console.log(startTime);
     console.log(endTime);
+
 
     //if all fields empty, present error. 
     if (selectedDate == "" && startTime == "" && endTime == "") {
@@ -98,18 +117,31 @@ Template.addAppointment.events({
 
     //call insert method when user inputs all fields or indicate whole day not free
     if (selectedDate != "" && startTime != "" && endTime != "" || selectedDate != "" && $('#availability').checkbox('is checked')) {
-      //popup message to let players confirm if they want to block out date
-      var confirmBlockDate = confirm("Are you sure you want to block out the date?");
-      if (confirmBlockDate == true) { //player confirm want to block out date
-        Meteor.call( 'indicateAvailability', selectedDate, startTime, endTime, (error,response) => {
-          if ( error ) {
-            Bert.alert( error.reason, 'danger' );
-          } else {
-            Bert.alert( 'Date blocked out', 'success' );
-          }
-        });
+        
+        if (Session.get('selectedBlockOut') != null) { //user did click on edit button and they decide to confirm edit 
+          var id = Session.get('oldEntry'); 
+          console.log(id);
+          Meteor.call('blockout.remove',id); //remove the old entry & update with new one
+          document.getElementById("blockout").style.display="block";
+          document.getElementById("confirm").style.display="none";
+          document.getElementById("cancel").style.display="none";
+        }
 
-      }
+        if (startTime >= endTime) { 
+          Bert.alert('Start Time need to be before End Time!', 'danger')
+        } else {
+          if (!Session.equals('selectedBlockOut', null)) { 
+            Session.set('selectedBlockOut', null);
+          }    
+
+          Meteor.call( 'indicateAvailability', selectedDate, startTime, endTime, (error,response) => {
+            if ( error ) {
+              Bert.alert( error.reason, 'danger' );
+            } else {
+              Bert.alert( 'Date blocked out', 'success' );
+            }
+          });  
+        }
     }
   }
 });
