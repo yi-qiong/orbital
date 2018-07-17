9733f52fa41603e8bc91530d86e2d2e518fba5c8
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-07-17 05:26:31 -0700

    update

--- client/templates/availability.js
+++ client/templates/availability.js
@@ -1,7 +1,9 @@
-import {Meteor} from 'meteor/meteor';
-import {Template } from 'meteor/templating';
-import {Availability} from '/imports/api/availability.js'
-
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+import { $ } from 'meteor/jquery';
+import dataTablesBootstrap from 'datatables.net-bs';
+import 'datatables.net-bs/css/dataTables.bootstrap.css';
+dataTablesBootstrap(window, $);
 
 Template.appointments.onCreated(function() {
   this.autorun(() => {
@@ -9,29 +11,4 @@ Template.appointments.onCreated(function() {
   });
 });
 
-Template.appointments.events({
-  'click #edit': function() {
-  //need to present the info in the picker then allow user to change the info
-    Session.set('selectedBlockOut',this); //pasing the entire appointment object
-    Session.set('oldEntry', this._id); //to allow this entry to be removed when user edit current entry
-  },
 
-  'click #delete': function() {
-  //delete the data
-    Meteor.call('blockout.remove',this._id);
-  },
-});
-
-Template.appointments.helpers({
-  appointments() {
-    return Availability.find({owner: Meteor.userId()}).fetch();
-	//console.log(appointments);
-  },
-  string(){
-  	if (this.start == "" && this.end =="") {
-      return "on " + this.date;
-    } else {
-      return "on " + this.date + " from " + this.start + " to " + this.end;
-    }
-  },
-});
