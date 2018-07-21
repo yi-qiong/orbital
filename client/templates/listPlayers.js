c1b6dbf9456fa845694adff9ce4e8f9096c7f97c
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-07-21 02:14:06 -0700

    list players registered by convening

---
+++ client/templates/listPlayers.js
@@ -0,0 +1,11 @@
+import {Meteor} from 'meteor/meteor';
+import {Template } from 'meteor/templating';
+import './listPlayers.html';
+import'/imports/api/registeredPlayers.js';
+
+Router.route('/registerPlayer', {
+  template: 'listPlayers'
+}); 
+
+
+
