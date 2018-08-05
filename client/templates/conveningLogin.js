530057d88badf58a11ac73fedbd8fc160aef37f4
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-08-05 03:37:06 -0700

    update

--- client/templates/conveningLogin.js
+++ client/templates/conveningLogin.js
@@ -2,6 +2,25 @@ import { Template } from 'meteor/templating';
 import './conveningLogin.html';
 import '/imports/api/registeredConvening.js';
 
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+    $("#login-form").delay(100).fadeIn(100);
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+  $('#register-form-link').click(function(e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+});
+
 //Meteor.subscribe('allUsers');
 Template.conveningLogin.onCreated(function() {
   this.autorun(() => {
@@ -16,22 +35,6 @@ Router.route('/conveningLogin', {
 
 Template.conveningLogin.events({
 
-  'click #login-form-link': function(event) {
-    $("#login-form").delay(100).fadeIn(100);
-    $("#register-form").fadeOut(100);
-    $('#register-form-link').removeClass('active');
-    $(this).addClass('active');
-    event.preventDefault();
-  },
-
-  'click #register-form-link': function(event) {
-    $("#register-form").delay(100).fadeIn(100);
-    $("#login-form").fadeOut(100);
-    $('#login-form-link').removeClass('active');
-    $(this).addClass('active');
-    event.preventDefault();
-  },
-
   'submit #login-form': function(event) {
     event.preventDefault();
     var conveningEmail = event.target.loginEmail.value;
@@ -43,6 +46,7 @@ Template.conveningLogin.events({
         Bert.alert('Login forbidden!', 'danger')
       } else {
         Router.go("/scheduling");
+        window.location.reload(true);
       }
     });
   },
@@ -57,7 +61,7 @@ Template.conveningLogin.events({
     console.log(commonPassword);
 
     if (commonPassword != "4-c0nVen1nG-0nLY") {
-      Bert.alert('You have entered a wrong password!', 'danger')
+      Bert.alert('Register denied, wrong convening password!', 'danger')
     } else {
       Meteor.call('insertConvening', conveningEmail, conveningName, (error, response) => { //store into register convening collection to be retrieved upon register player
           if (error) {
@@ -74,6 +78,7 @@ Template.conveningLogin.events({
                             } else {
                               console.log(Meteor.user());
                                 Router.go("/scheduling");
+                                window.location.reload(true);
                             }
                         });
                     }
