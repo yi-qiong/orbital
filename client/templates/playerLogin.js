544161c53b3ed807c625746bb4bddda80a37cbdb
CharleneChia <39288967+CharleneChia@users.noreply.github.com>
2018-08-05 03:43:23 -0700

    update

--- client/templates/playerLogin.js
+++ client/templates/playerLogin.js
@@ -1,18 +1,29 @@
 import { Template } from 'meteor/templating';
 import './playerLogin.html';
+import {RegisteredPlayers} from '/imports/api/registeredPlayers.js';
 
 Router.route('/playerLogin', {
 	template: 'playerLogin'
 });
 
-Template.playerLogin.events({
+Meteor.subscribe('registeredPlayers');
 
+Template.playerLogin.events({
+  'click #signUp': function(event) {
+    Router.go('/player-register');
+    window.location.reload(true)
+  },
   'submit #contact-form': function(event) {
     event.preventDefault();
     var playerEmail = event.target.email.value;
     var playerPassword = event.target.password.value;
 
-    Meteor.loginWithPassword(playerEmail, playerPassword, function(error){
+    var isRegistered = RegisteredPlayers.findOne({email: playerEmail});
+            console.log(isRegistered);
+            if (isRegistered == undefined) {
+                Bert.alert('Login forbidden!')
+            } else {
+              Meteor.loginWithPassword(playerEmail, playerPassword, function(error){
       if(error){
         console.log(error.reason);
         Bert.alert('Login forbidden!', 'danger')
@@ -20,5 +31,6 @@ Template.playerLogin.events({
         Router.go("/playerPage");
       }
     });
+    }
   },
 });
